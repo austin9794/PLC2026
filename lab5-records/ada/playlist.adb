dd9e4e2c4b87405bb4a722ced397990c2ebea23b
Umair <111871488+austin9794@users.noreply.github.com>
2026-02-16 14:59:03 +0000

    uncomment 39-43 and 48-49

--- lab5-records/ada/playlist.adb
+++ lab5-records/ada/playlist.adb
@@ -36,15 +36,15 @@ procedure PlayList is
         length_secs => 17.0*60.0+26.0
        );
 
-    -- pause1 : Item :=
-    --    (
-    --     item_variant => PAUSE,
-    --     length_secs => 5.0
-    --    );
+     pause1 : Item :=
+        (
+         item_variant => PAUSE,
+         length_secs => 5.0
+        );
 
 begin
     Put_Item (piece1);
     Put_Line ("");
-    -- Put_Item(pause1);
-    -- Put_Line ("");
+    Put_Item(pause1);
+    Put_Line ("");
 end PlayList;
